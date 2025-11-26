214e4e5ecacfda76d26d74d7bcc79f03f54f95f1
suleymangursooy <suleyman.gursoy@migrosonline.com>
2025-11-26 16:36:43 +0300

    kick-off

---
+++ src/test/java/com/sgursoy/race_condition/RaceConditionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgursoy.race_condition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceConditionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
